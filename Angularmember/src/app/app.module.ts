bf71ea75ce0eee2712faf6b69d03124cc597c1b1
zanzanzanm03 <olafminister12365@hotmail.com>
2021-04-20 21:59:05 +0700

    add

--- Angularmember/src/app/app.module.ts
+++ Angularmember/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angularmember/src/assets/js/main.js
+++ Angularmember/src/assets/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 var App = {
-	initialLoadPage(){
+	initialLoadPage: function(){
 		var treeviewMenu = $('.app-menu');
 
 	// Toggle Sidebar
