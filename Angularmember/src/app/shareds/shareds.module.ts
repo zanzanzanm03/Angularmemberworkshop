ba9cf04643d14dcc6ff00844307c1764c5b7a52d
zanzanzanm03 <olafminister12365@hotmail.com>
2021-04-22 00:45:07 +0700

    add member

--- Angularmember/src/app/shareds/shareds.module.ts
+++ Angularmember/src/app/shareds/shareds.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BsDropdownModule, ModalModule } from 'ngx-bootstrap';
+import { BsDropdownModule, ModalModule, PaginationModule } from 'ngx-bootstrap';
 import { AuthNavbarComponent } from './components/auth-navbar/auth-navbar.component';
 import { AuthSidebarComponent } from './components/auth-sidebar/auth-sidebar.component';
 import { AuthContentComponent } from './components/auth-content/auth-content.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AlertService } from './services/alert.service';
-import { AccountService } from './services/account.service';
 import { ValidatorsService } from './services/validators.service';
 
 
@@ -19,6 +18,7 @@ import { ValidatorsService } from './services/validators.service';
     FormsModule,
     BsDropdownModule.forRoot(),
     ModalModule.forRoot(),
+    PaginationModule.forRoot()
   ],
   declarations: [
     AuthNavbarComponent,
@@ -32,7 +32,8 @@ import { ValidatorsService } from './services/validators.service';
     AuthContentComponent,
     ReactiveFormsModule,
     FormsModule,
-    ModalModule
+    ModalModule,
+    PaginationModule
   ],
   providers: [
     AlertService,
