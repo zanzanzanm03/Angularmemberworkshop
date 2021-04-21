89c6a5ddc7dddafb7d481c187d93f816241c63d6
zanzanzanm03 <olafminister12365@hotmail.com>
2021-04-21 15:11:28 +0700

    add

--- Angularmember/src/app/components/register/register.component.ts
+++ Angularmember/src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppURL } from '../../app.url';
 import { IRegisterComponent } from './register.interface';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { AlertService } from 'src/app/shareds/services/alert.service';
 declare let $;
 @Component({
@@ -32,11 +32,24 @@ export class RegisterComponent implements IRegisterComponent {
     this.form = this.builder.group({
       firstname: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-      cpassword: ['', [Validators.required]]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^[A-z0-9]{6,15}$/)]],
+      cpassword: ['', [Validators.required, this.comparePassword('password')]]
     });
   }
 
   // สร้าง validate เอง
+  private comparePassword(passwordField: string) {
+    return function (confirm_password: AbstractControl) {
+      if (!confirm_password.parent) return;
+      const password = confirm_password.parent.get(passwordField);
+      const passwordSubscripe = password.valueChanges.subscribe(() => {
+        confirm_password.updateValueAndValidity();
+        passwordSubscripe.unsubscribe();
+      });
+      if (confirm_password.value === password.value)
+        return;
+      return { compare: true };
+    }
+  }
 }
