be77064f5f7c9b199131298bbf893a81635eb163
zanzanzanm03 <olafminister12365@hotmail.com>
2021-04-21 18:37:40 +0700

    add

--- Angularmember/src/app/shareds/components/auth-navbar/auth-navbar.component.ts
+++ Angularmember/src/app/shareds/components/auth-navbar/auth-navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenService } from 'src/app/services/authen.service';
 import { AppURL } from '../../../app.url';
 import { AuthURL } from '../../../authentication/authentication.url';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-auth-navbar',
@@ -9,11 +12,23 @@ import { AuthURL } from '../../../authentication/authentication.url';
 })
 export class AuthNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authen: AuthenService,
+    private alert: AlertService
+  ) { }
 
   ngOnInit() {
   }
 
   AppURL = AppURL;
   AuthURL = AuthURL;
+
+  // การออกจากระบบ
+  onLogout() {
+    this.alert.notify('ออกจากระบบเรียบร้อย', 'Info');
+    this.authen.clearAuthenticated();
+    this.router.navigate(['/', AppURL.Login]);
+
+  }
 }
