3327a2700c63641051fa818ab76d9061de65f519
zanzanzanm03 <olafminister12365@hotmail.com>
2021-04-21 22:38:37 +0700

    add chpass3

--- Angularmember/src/app/authentication/components/profile/change-password/change-password.component.ts
+++ Angularmember/src/app/authentication/components/profile/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { IChangePasswordComponent } from './change-password.interface';
 import { BsModalRef } from 'ngx-bootstrap';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
@@ -8,7 +9,26 @@ import { BsModalRef } from 'ngx-bootstrap';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements IChangePasswordComponent {
-  constructor() { }
+  constructor(
+    private builder: FormBuilder
+  ) {
+    this.initialCreateFormData();
+  }
 
   @Input('modalRef') modalRef: BsModalRef;
+  form: FormGroup;
+
+  // เปลี่ยนรหัสผ่าน
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  // สร้างฟอร์ม
+  private initialCreateFormData() {
+    this.form = this.builder.group({
+      old_pass: [],
+      new_pass: [],
+      cnew_pass: []
+    });
+  }
 }
--- Angularmember/src/app/authentication/components/profile/change-password/change-password.interface.ts
+++ Angularmember/src/app/authentication/components/profile/change-password/change-password.interface.ts
@@ -1,5 +1,9 @@
+import { FormControlName, FormGroup } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap';
 
 export interface IChangePasswordComponent {
     modalRef: BsModalRef;
+    form: FormGroup;
+
+    onSubmit();
 }
