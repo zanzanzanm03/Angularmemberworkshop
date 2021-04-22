dd22ca9e75b989bcbdce24a1b27c0f1075cd5df6
zanzanzanm03 <olafminister12365@hotmail.com>
2021-04-22 16:26:03 +0700

    add

--- Angularmember/src/app/app.routing.ts
+++ Angularmember/src/app/app.routing.ts
@@ -3,11 +3,12 @@ import { AppURL } from './app.url';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthenticationGuard } from './guards/authentication.guard';
+import { UnauthenticationGuard } from './guards/unauthentication.guard';
 
 const RouteLists: Routes = [
     { path: '', redirectTo: AppURL.Login, pathMatch: 'full' },
-    { path: AppURL.Login, component: LoginComponent },
-    { path: AppURL.Register, component: RegisterComponent },
+    { path: AppURL.Login, component: LoginComponent, canActivate: [UnauthenticationGuard] },
+    { path: AppURL.Register, component: RegisterComponent, canActivate: [UnauthenticationGuard] },
     {
         path: AppURL.Authen,
         loadChildren: './authentication/authentication.module#AuthenticationModule',
